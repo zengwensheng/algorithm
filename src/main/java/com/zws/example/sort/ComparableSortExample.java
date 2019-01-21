2223c89ff9b8df2c4150aa3cb60c65896965a750
zws <zengwensheng>
2019-01-21 16:57:55 +0800

    shell排序

--- src/main/java/com/zws/example/sort/ComparableSortExample.java
+++ src/main/java/com/zws/example/sort/ComparableSortExample.java
@@ -11,9 +11,9 @@ public class ComparableSortExample {
 
     public static void main(String[] args) {
 
-        int n = 10000;
+        int n = 100000;
         int rangeL = 0;
-        int rangeR = 10000;
+        int rangeR = 100000;
         int swapTimes = 10;
 
         Integer [] selectionArray = SortUtil.generateRandomArray(n,rangeL,rangeR);
@@ -30,17 +30,14 @@ public class ComparableSortExample {
         selectionArray = SortUtil.generateNearlyOrderedArray(n, swapTimes);
         insertionArray = selectionArray.clone();
         bubbleArray = selectionArray.clone();
+        shellArray = selectionArray.clone();
 
         SortUtil.calculateTime("Optimal: selection sort",SelectionSortExample::selectionSort,selectionArray);
         SortUtil.calculateTime("Optimal: insertion advance2 sort", InsertionSortExample::insertionSortAdvance2, insertionArray);
         SortUtil.calculateTime("Optimal: bubble advance2 sort",BubbleSortExample::bubbleSortAdvance2,bubbleArray);
+        SortUtil.calculateTime("Optimal: shell sort",ShellSortExample::shellSort,shellArray);
 
 
-        selectionArray = SortUtil.generateNearlyOrderedArrayFromBigToSmall(n, swapTimes);
-        insertionArray = selectionArray.clone();
-        shellArray = selectionArray.clone();
 
-        SortUtil.calculateTime("from big to small: insertion advance2 sort", InsertionSortExample::insertionSortAdvance2, insertionArray);
-        SortUtil.calculateTime("from big to small: shell sort", ShellSortExample::shellSort, shellArray);
     }
 }
--- src/main/java/com/zws/example/sort/ShellSortExample.java
+++ src/main/java/com/zws/example/sort/ShellSortExample.java
@@ -6,33 +6,45 @@ import com.zws.util.SortUtil;
  * @author zws
  * @email 2848392861@qq.com
  * date 2019/1/17
- *
- * sheel 排序
- *
- *
+ * <p>
+ * shell 排序
+ *    希尔排序是把记录按下标的一定增量分组，对每组使用直接插入排序算法排序；随着增量逐渐减少，每组包含的关键词越来越多，当增量减至1时，整个文件恰被分成一组，算法便终止
+ * 不稳定
  */
 public class ShellSortExample {
 
     public static void main(String[] args) {
-        int n = 100000;
+        int n = 10;
         int rangeL = 0;
-        int rangeR = 100000;
+        int rangeR = 10;
         int swapTimes = 10;
 
         Integer[] arrays = SortUtil.generateRandomArray(n, rangeL, rangeR);
 
-        /**
-         * 最差测试
-         */
-        SortUtil.calculateTime("Worst: shell sort", ShellSortExample::shellSort, arrays);
         SortUtil.printArray(arrays);
+        SortUtil.calculateTime("Worst: shell sort", ShellSortExample::shellSort, arrays);
+        //  SortUtil.printArray(arrays);
+
     }
 
 
     /**
-     *  sheel 排序  从小到大
+     * shell 排序  从小到大
      *
-     * 代码                                     执行次数                        声明变量次数
+     * 假如这个数组10个元素   n=10
+     *  当增量等于2时 那么d=n/2=5
+     *        插入排序比较次数:   (1)*d =5
+     *
+     *  当增量等于4时 那么d=n/4=2
+     *        插入排序比较次数（等差数列求和)等于: (1+2+3+4+5)*d=15*2 = 30
+     *
+     *  当增量等于8时 那么d=n/8=1
+     *        插入排序比较次数（等差数列求和)等于: (1+2+3+4+5+6+7+8+9+10)*d=55*1 = 55
+     *  得出
+     *    (1+n/d)*(n/d)/2*d
+     *
+     *
+     *  时间复杂度： O(n^(1.3——2))
      *
      * @param comparableArray
      */
@@ -41,9 +53,9 @@ public class ShellSortExample {
         for (int d = comparableArray.length / 2; d > 0; d /= 2) {
             for (int i = 0; i < d; i++) {
                 for (int j = i; j + d < comparableArray.length; j += d) {
-                    int k = j+d;
+                    int k = j + d;
                     indexComparable = comparableArray[k];
-                    for (; k-d >= 0 && indexComparable.compareTo(comparableArray[k - d]) == -1; k -= d) {
+                    for (; k - d >= 0 && indexComparable.compareTo(comparableArray[k - d]) == -1; k -= d) {
                         comparableArray[k] = comparableArray[k - d];
                     }
                     comparableArray[k] = indexComparable;
@@ -53,6 +65,8 @@ public class ShellSortExample {
         }
 
     }
+
+
 
 
 }
