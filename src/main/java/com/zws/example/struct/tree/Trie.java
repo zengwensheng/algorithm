9f8bdc26a10f175901460a38581e81e403bead86
zws <2848392861@qq.com>
2019-05-05 18:17:09 +0800

    数据结构

---
+++ src/main/java/com/zws/example/struct/tree/Trie.java
@@ -0,0 +1,18 @@
+package com.zws.example.struct.tree;
+
+/**
+ * @author zws
+ * @email 2848392861@qq.com
+ * date 2019/5/5
+ *
+ *  Trie 前缀树，字典树
+ *    实现字典
+ *        二分搜素树                                       Trie
+ *      如果有n个条目                             查询每个条目的时间复杂度
+ *      使用树结构                                和字典中一共有多少条目无关
+ *      查询的时间复杂度是O(logn)                  时间复杂度为O(w)
+ *      如果有100万个条目（2^20）                  w为查询单次的长度！
+ *      logn 大约为20                            大多数单次的长度小于10
+ */
+public class Trie {
+}
